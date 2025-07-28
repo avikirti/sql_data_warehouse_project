c64cd985872ad76301be4a98f347c4cbcc6bc12e
Avinash Kirti <150352386+avikirti@users.noreply.github.com>
2025-07-28 20:15:33 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,41 +1,42 @@
 /*
-============================================================
+=============================================================
 Create Database and Schemas
-============================================================
-
+=============================================================
 Script Purpose:
-    This script creates a new database named 'DataWarehouse' after checking if it already exists.
-    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
     within the database: 'bronze', 'silver', and 'gold'.
-
+	
 WARNING:
-    Running this script will drop the entire 'DataWarehouse' database if it exists.
-    All data in the database will be permanently deleted. Proceed with caution
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
     and ensure you have proper backups before running this script.
 */
 
-use master;
+USE master;
 GO
 
 -- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases where name='DataWarehouse')
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
-	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE DataWarehouse;
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
 END;
 GO
 
 -- Create the 'DataWarehouse' database
-create database DataWarehouse;
+CREATE DATABASE DataWarehouse;
 GO
 
-use datawarehouse;
+USE DataWarehouse;
 GO
 
---Create Schemas
-create schema bronze;
+-- Create Schemas
+CREATE SCHEMA bronze;
 GO
-create schema silver;
+
+CREATE SCHEMA silver;
 GO
-create schema gold;
+
+CREATE SCHEMA gold;
 GO
